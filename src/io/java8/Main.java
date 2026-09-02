a117c74fb335e32f0a148622e2c6f34ecdd9c433
Mohd Khizar Rasheed <161632919+mohdkhizarrasheed-7@users.noreply.github.com>
2026-09-02 12:57:05 +0530

    Anonymus class for implementing Interface

--- src/io/java8/Main.java
+++ src/io/java8/Main.java
@@ -4,8 +4,34 @@ public class Main
 {
 public static void main(String[] args) 
 {
-	Greet greet = new Greet();
-	greet.sayHello();
+//	Greet greet = new Greet();
+//	greet.sayHello();
 	
+	
+	
+	
+	//first anonymus class
+	Greeting greeting = new Greeting() 
+	{
+		@Override
+		public void sayHello()
+		{
+			System.out.println("Saying hello for the first time...");
+		}
+	};
+	greeting.sayHello();
+	
+	
+	
+	//second Anonymus class
+	Greeting greeting2 = new Greeting() 
+	{
+		@Override
+		public void sayHello()
+		{
+			System.out.println("Saying hello for the Second Time...");
+		}
+	};
+	greeting2.sayHello();
 }
 }
